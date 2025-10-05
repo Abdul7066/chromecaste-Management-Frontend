310cdb7310ffbb525a5fb19a34f89ba2c7e5542f
Md Abdul Raheman <mdabdulrahemanmdamjed@gmail.com>
2025-10-05 06:17:46 +0530

    add

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const VITE_API_BASE_URL = "http://localhost:5000";
+const VITE_API_BASE_URL = "http://chromecaste-Management.onrender.com/api";
 const api = {
   login: async (credentials) => {
     const response = await axios.post(
